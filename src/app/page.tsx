fa083e727ada797614bdfe0a266a712c87043ee3
humaafatima <humaf1904@gmail.com>
2024-10-29 22:28:54 +0500

    Adding a figma design through Next Js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,95 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+"use client"
+export default function home() {
+  return <div className="bg-black h-screen">
+    
+    <nav className="flex justify-between items-center bg-gray-800 p-4">
+ 
+  <div className="flex gap-4 list-none">
+    
+  <li className="text-white font-Montserrat text 50xl">BrandName</li>
+  </div>
+  <div className="flex gap-4 list-none">
+    <li className="text-white font-Montserrat text 14xl">Home</li>
+    <li className="text-white font-Montserrat text 14xl">Product</li>
+    <li className="text-white font-Montserrat text 14xl">Pricing</li>
+    <li className="text-white font-Montserrat text 14xl">Contact</li>
+    
     </div>
-  );
+
+  <div className="flex gap-4 list-none">
+  
+    <li className="text-white font-Montserrat">Login</li>
+    <li className="text-white font-Montserrat">JOIN US</li>
+    
+    
+  </div>
+</nav>
+<br>
+</br>
+<br>
+</br>
+<p className="font-extrabold text-2xl font-Montserrat text-center text-blue-400"> Welcome</p>
+<br></br>
+<h1 className="font-extrabold text-4xl font-Montserrat text-center py-6 text-white"> Selling on the internet like a pro</h1>
+<br></br>
+<h4 className="font-extrabold text font-Montserrat text-center text-white"> We know how large objects will act, but things on a <br>
+</br>
+  small scale just do not act that way.</h4>
+  <br></br>
+  <div className="flex justify-center items-center">
+      <button
+        onClick={() => alert("Button clicked!")}
+        className="mr-5 px-10 py-2 font-Montserrat border: 1px solid #23A6F0 text-white rounded hover:bg-blue-400">
+      Get Quote Now
+      </button>
+      <button
+        onClick={() => alert("Button clicked!")}
+  
+        className="px-10 py-2 font-Montserrat border: 1px solid #23A6F0 text-white rounded hover:bg-blue-400">
+      Learn More
+      </button>
+    </div>
+    <br></br>
+    <div className="flex gap-4 justify-center items-center">
+  <div className="w-80 h-80 bg-white text-black p-4">
+  <div className="flex gap-4 w-20 h-20 bg-red-300 rounded-md">
+
+</div>
+<br></br>
+    <p className="font-bold">training courses</p>
+    <br></br>
+    <hr className="w-8 border-t-2 border-red-500"></hr>
+    <br></br>
+    <p className="text-black">The gradual accumulation of 
+information about atomic and 
+small-scale behaviour...</p>
+  </div>
+  <div className="w-80 h-80 bg-white text-black p-4">
+  <div className="flex gap-4 w-20 h-20 bg-green-300 rounded-md">
+
+</div>
+<br></br>
+<p className="font-bold">2,769 online courses</p>
+    <br></br>
+    <hr className="w-8 border-t-2 border-red-500"></hr>
+    <br></br>
+    <p className="text-black">The gradual accumulation of 
+information about atomic and 
+small-scale behaviour...</p>
+  </div>
+  <div className=" font-bold w-80 h-80 bg-blue-500 text-white p-4">
+  <div className="flex gap-4 w-20 h-20 bg-white rounded-md">
+
+</div>
+<br></br>
+<p className="font-bold">training courses</p>
+    <br></br>
+    <hr className="w-8 border-t-2"></hr>
+    <br></br>
+    <p className="text-white">The gradual accumulation of 
+information about atomic and 
+small-scale behaviour...</p>
+  </div>
+</div>
+</div>
 }
